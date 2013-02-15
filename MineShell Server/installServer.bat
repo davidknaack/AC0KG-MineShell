6dc6fc35f545bd32972377674cef795c44cfe92c
davidknaack <davidknaack@gmail.com>
2013-02-14 21:17:17 -0600

    unnecessary

--- MineShell Server/installServer.bat
+++
@@ -1,5 +0,0 @@
-installutil /i "MineCraft Server.exe"
-cls
-echo Server installed :)
-echo run server.jar first without starting the service to create a new world, create all settings files and make yourself OP
-pause
